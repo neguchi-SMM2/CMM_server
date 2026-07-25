bb0cc045dae8e2f6df4246e4b714c3d2cdf23ec9
Neguchi <yuy.gyusuji623@gmail.com>
2026-07-25 23:05:05 +0900

    Update database.js

--- database.js
+++ database.js
@@ -161,7 +161,7 @@ const INFO_COLS = `id, title, author, like_count, play_count, attempt_count, cle
 
 async function getRandomCourses(limit) {
   const { rows } = await pool.query(
-    `SELECT ${INFO_COLS} FROM courses ORDER BY posted_at + (RANDOM() * 2880) DESC LIMIT $1`, [limit]
+    `SELECT ${INFO_COLS} FROM courses ORDER BY posted_at + (RANDOM() * 21600) DESC LIMIT $1`, [limit]
   );
   return rows;
 }
